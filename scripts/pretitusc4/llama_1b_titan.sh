bddab82666c1f32ac1f95071864f9facdab0e503
Incentivus <tenyearsproject5@gmail.com>
2025-09-10 15:35:02 +0300

    Create llama_1b_titan.sh

---
+++ scripts/pretitusc4/llama_1b_titan.sh
@@ -0,0 +1,25 @@
+# LLaMA-1B, Titan, 8 A100, 1 Node
+num_rank=256
+scale_type=channel
+proj_type=random
+titan_scale=1
+
+torchrun --standalone --nproc_per_node 8 main_pretrain.py \
+    --model_config configs/llama_1b.json \
+    --eval_every 1000 \
+    --dtype bfloat16 \
+    --batch_size 64 \
+    --total_batch_size 512 \
+    --lr 0.01 \
+    --warmup_steps 10000 \
+    --num_training_steps 100000 \
+    --optimizer titan_adamw \
+    --titan_scale ${titan_scale} \
+    --rank ${num_rank} \
+    --scale_type ${scale_type} \
+    --proj ${proj_type} \
+    --update_proj_gap 200 \
+    --weight_decay 0 \
+    --project titan_test \
+    --name titan_test_titan_1b \
+    --save_dir ./ckpts/TitusW_7b_scale${titan_scale}_rank${num_rank}_proj${proj_type}_type${scale_type}
