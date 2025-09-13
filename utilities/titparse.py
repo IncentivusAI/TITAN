44c5f5d4034412f72d1e387dd1647d6468b4c2a5
Incentivus <tenyearsproject5@gmail.com>
2025-09-13 12:36:40 +0300

    Update titparse.py

--- utilities/titparse.py
+++ utilities/titparse.py
@@ -91,4 +91,30 @@ def max_train_tokens_to_number(max_train_tokens):
 
 def check_args_torchrun_main(args):
 
+    if args.save_dir is None:
+        # create checkpoints / model name with timestamp
+        args.save_dir = f"checkpoints/{args.model_config.split('/')[-1].rstrip('.json')}-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
 
+    os.makedirs(args.save_dir, exist_ok=True)
+
+    if args.tags is not None:
+        args.tags = args.tags.split(",")
+
+    if args.total_batch_size is None:
+        args.gradient_accumulation = args.gradient_accumulation or 1
+        args.total_batch_size = args.batch_size * args.gradient_accumulation
+
+    assert args.total_batch_size % args.batch_size == 0, "total_batch_size must be divisible by batch_size"
+
+    if args.max_train_tokens is not None:
+        args.num_training_steps = args.max_train_tokens // args.total_batch_size
+        logger.info(f"Training for {args.num_training_steps} update steps")
+
+    if args.continue_from is not None:
+        assert os.path.exists(args.continue_from), f"--continue_from={args.continue_from} does not exist"
+
+    if args.dtype in ["fp16", "float16"]:
+        raise NotImplementedError("fp16 not supported in torchrun_main.py. Use deepspeed_main.py instead (though it may have issues).")
+
+    return args
+```
